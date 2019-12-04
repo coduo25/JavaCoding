3f800e3a861412b5d92739be11fabff667ce3b90
coduo25 <coduo25@gmail.com>
2019-12-04 09:56:06 +0900

    Ex74

---
+++ src/Basic1/Ex74.java
@@ -0,0 +1,15 @@
+package Basic1;
+
+public class Ex74 {
+
+	//Write a Java program to test if 10 appears as either the first or last element 
+	//of an array of integers. The length of the array must be greater than or equal to 2.
+	public static void main(String[] args) {
+		
+		int[] number = {10, -20, 0, 30, 40, 60, 10};
+
+		System.out.println(number[0] == 10 || number[number.length-1] == 10);
+	
+	}
+
+}
